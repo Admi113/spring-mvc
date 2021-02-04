48e86f83864d65e43c04f47cd91433989f37d4ec
admi113 <nika1pb113@gmail.com>
2021-02-04 16:37:37 +0300

    2.2.2_04.02.2021.1

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarController.java
@@ -3,16 +3,9 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarService;
 import web.service.CarServiseImpl;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
@@ -22,7 +15,7 @@ public class CarController {
     @GetMapping(value = "/Cars")
     public String printWelcome(@RequestParam int id, Model model) {
         model.addAttribute("messages", carServise.getCarList(id));
-        return "carView";
+        return "cars";
     }
 
 }
